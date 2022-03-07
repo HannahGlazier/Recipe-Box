544d519d6072151e631e04cecb3f7ddf9b6f5133
Hannah Glazier <hrayglazier@aol.com>
2022-03-07 08:39:36 -0700

    reconfig initial data.json

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import React, { useEffect } from 'react'
 import Header from './components/Header'
 import RecipeContainer from './components/RecipeContainer'
+ 
 
 function App() {
+
+  useEffect(() => {
+    fetch('./data.json')
+    .then(response => response.json())
+    .then(console.log)
+  })
+
   return (
     <div>
-      <Header />
+      <Header  
+      />
       <RecipeContainer />
     </div>
   );
