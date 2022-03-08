f76d7ccc06444a5d152418a2f535ebd088857287
Hannah Glazier <hrayglazier@aol.com>
2022-03-08 08:30:04 -0700

    added github links

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { SiGithub } from "react-icons/si"
 
 function About() {
   return (
+    <div className='about'>
     <div>About</div>
+    <a href="https://github.com/HannahGlazier" target="_blank" rel="noreferrer"><SiGithub />Hannah Glazier</a>
+    <br></br>
+    <a href="https://github.com/8eth" target="_blank" rel="noreferrer" ><SiGithub />Beth Fekadu</a>
+    </div>
   )
 }
 
